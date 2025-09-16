497b6564093c84825b68d0932bb2df44874a34fb
Krati1105 <agrakrati123@gmail.com>
2025-09-16 11:59:51 +0530

    Create  Vehicle Registration.java

---
+++ java Constructors/Instance /Vehicle Registration.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.oops.levelone;
+
+class Vehicle {
+    private String ownerName;
+    private String vehicleType;
+    private static double registrationFee = 500.0;
+
+    public Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    public void displayVehicleDetails() {
+        System.out.println("Owner: " + ownerName + ", Vehicle Type: " + vehicleType + ", Registration Fee: " + registrationFee);
+    }
+
+    public static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle("Krati Agrawal", "Car");
+        Vehic
