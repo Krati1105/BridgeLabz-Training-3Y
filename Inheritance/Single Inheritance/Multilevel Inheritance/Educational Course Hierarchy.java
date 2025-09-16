de2c6f189321a83d0bf9bbdca011eade492027f0
Krati1105 <agrakrati123@gmail.com>
2025-09-16 21:32:53 +0530

    Create  Educational Course Hierarchy.java

---
+++ Inheritance/Single Inheritance/Multilevel Inheritance/Educational Course Hierarchy.java
@@ -0,0 +1,30 @@
+class Course {
+    String courseName;
+    int duration; // in weeks
+
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+
+    public void displayCourseInfo() {
+        System.out.println("Course Name: " + courseName);
+        System.out.println("Duration: " + duration + " weeks");
+    }
+}
+
+class OnlineCourse extends Course {
+    String platform;
+    boolean isRecorded;
+
+    public OnlineCourse(String courseName, int duration, String platform, boolean isRecorded) {
+        super(courseName, duration);
+        this.platform = platform;
+        this.isRecorded = isRecorded;
+    }
+
+    @Override
+    public void displayCourseInfo() {
+        super.displayCourseInfo();
+        System.out.println("Platform: " + platform);
+        System.out.println("Recorded: " + (isR
