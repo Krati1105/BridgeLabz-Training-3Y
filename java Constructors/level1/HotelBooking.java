ca98662f6b7b1de681537e200b7a5bcb8a29cfb7
Krati1105 <agrakrati123@gmail.com>
2025-09-16 11:56:22 +0530

    Create HotelBooking.java

---
+++ java Constructors/level1/HotelBooking.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.oops.levelone;
+
+class HotelBooking {
+    private String guestName;
+    private String roomType;
+    private int nights;
+
+    public HotelBooking() {
+        this.guestName = "Unknown";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    public HotelBooking(HotelBooking booking) {
+        this.guestName = booking.guestName;
+        this.roomType = booking.roomType;
+        this.nights = booking.nights;
+    }
+
+    public void display() {
+        System.out.println("Guest: " + guestName + ", Room: " + roomType + ", Nights: " + nights);
+    }
+
+    public static void main(String[] args) {
+        HotelBooking h1 = new HotelBooking();
+        HotelBooking h2 = new HotelBooking("Bob", "Deluxe", 3);
+        HotelBooking h3 = new HotelBooking(h2);
+        h1.display();
+        h2.display();
+        h3.display();
+    }
+}
