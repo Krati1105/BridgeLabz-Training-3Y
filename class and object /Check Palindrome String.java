3b64207c9b656871a830408c4abc18fc7506fb25
Krati1105 <agrakrati123@gmail.com>
2025-09-16 11:47:00 +0530

    Create Check Palindrome String.java

---
+++ class and object /Check Palindrome String.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.oops.levelone;
+
+class PalindromeChecker {
+    private String text;
+
+    public PalindromeChecker(String text) {
+        this.text = text;
+    }
+
+    public boolean isPalindrome() {
+        String cleanedText = text.replaceAll("\\s+", "").toLowerCase();
+        int left = 0, right = cleanedText.length() - 1;
+
+        while (left < right) {
+            if (cleanedText.charAt(left) != cleanedText.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public void displayResult() {
+        if (isPalindrome()) {
+            System.out.println("\"" + text + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + text + "\" is not a palindrome.");
+        }
+    }
+}
+
+public class PalindromeCheckerMain {
+    public static void main(String[] args) {
+        PalindromeChecker p1 = new PalindromeChecker("Madam");
+        PalindromeChecker p2 = new PalindromeChecker("Hello");
+
+        p1.displayResult();
+        p2.displayResult();
+    }
+}
