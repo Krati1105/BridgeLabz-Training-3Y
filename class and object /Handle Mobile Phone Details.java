7ab417b751084a928045bf68673cdc658c34207a
Krati1105 <agrakrati123@gmail.com>
2025-09-16 11:41:23 +0530

    Create Handle Mobile Phone Details.java

---
+++ class and object /Handle Mobile Phone Details.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.oops.levelone;
+
+class MobilePhone {
+    private String brand;
+    private String model;
+    private double price;
+
+    public MobilePhone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: " + price);
+    }
+}
+
+public class MobilePhoneMain {
+    public static void main(String[] args) {
+        MobilePhone phone1 = new MobilePhone("Apple", "iPhone 15", 120000.0);
+        MobilePhone phone2 = new MobilePhone("Samsung", "Galaxy S24", 95000.0);
+
+        phone1.displayDetails();
+        System.out.println("----------------------");
+        phone2.displayDetails();
+    }
+}
